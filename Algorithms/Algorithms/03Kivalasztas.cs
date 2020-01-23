c32ea984cd0d8d7608ae359e72180120ebbd5b9f
Agoston <totha014@gmail.com>
2020-01-23 17:13:48 +0100

    Implement number 3 algorithm. Selection

---
+++ Algorithms/Algorithms/03Kivalasztas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    partial class Program
+    {
+        static int Kivalasztas(int[] x)
+        {
+            int i = 0;
+            while(!(x[i] > 5))
+            {
+                i += 1;
+            }
+            int index = i;
+
+            return index;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -17,14 +17,23 @@ namespace Algorithms
 
             #region Eldontes teszt
 
-            int[] tomb1 = new int[] { 1, 2 };
-            bool eredmeny1 = Eldontes(tomb1);
+            //int[] tomb1 = new int[] { 1, 2 };
+            //bool eredmeny1 = Eldontes(tomb1);
 
-            int[] tomb2 = new int[] { 1, 6 };
-            bool eredmeny2 = Eldontes(tomb2);
+            //int[] tomb2 = new int[] { 1, 6 };
+            //bool eredmeny2 = Eldontes(tomb2);
 
-            Console.WriteLine(eredmeny1);
-            Console.WriteLine(eredmeny2);
+            //Console.WriteLine(eredmeny1);
+            //Console.WriteLine(eredmeny2);
+
+            #endregion
+
+            #region Kivalasztas teszt
+
+            int[] tomb = new int[] { 1, 2, 3, 4, 5, 6 };
+            int eredmeny = Kivalasztas(tomb);
+
+            Console.WriteLine(eredmeny);
 
             #endregion
         }
