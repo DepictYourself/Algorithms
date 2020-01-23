da51d1f3ad109e9f1916b8afbb51ad3d82c397a5
Agoston <totha014@gmail.com>
2020-01-23 15:14:21 +0100

    Create C# solution and console project

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static int Sorozatszamitas(int[] tomb)
+        {
+            int eredmeny = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                eredmeny = eredmeny + tomb[i];
+            }
+
+            return eredmeny;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[] { 1, 3, 4, 5, 8 };
+            int osszeadasEredmenye = Sorozatszamitas(tomb);
+
+            Console.WriteLine(osszeadasEredmenye);
+        }
+    }
+}
