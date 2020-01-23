f0cb87f45577a6efd4338af3704c3cb1d8ae5066
Agoston <totha014@gmail.com>
2020-01-23 16:09:53 +0100

    Implement number 2: Eldontes algorithm

---
+++ Algorithms/Algorithms/02Eldontes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    partial class Program
+    {
+        static bool Eldontes(int[] x)
+        {
+            int i = 0;
+            while(i < x.Length && !(x[i] > 5))
+            {
+                i = i + 1;
+            }
+            bool van = i < x.Length;
+
+            return van;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -8,14 +8,25 @@ namespace Algorithms
         {
             #region Sorozatszamitas teszt
 
-            int[] tomb = new int[] { 1, 3, 4, 5, 8 };
-            int osszeadasEredmenye = Sorozatszamitas(tomb);
+            //int[] tomb = new int[] { 1, 3, 4, 5, 8 };
+            //int osszeadasEredmenye = Sorozatszamitas(tomb);
 
-            Console.WriteLine(osszeadasEredmenye);
+            //Console.WriteLine(osszeadasEredmenye);
 
             #endregion
 
+            #region Eldontes teszt
 
+            int[] tomb1 = new int[] { 1, 2 };
+            bool eredmeny1 = Eldontes(tomb1);
+
+            int[] tomb2 = new int[] { 1, 6 };
+            bool eredmeny2 = Eldontes(tomb2);
+
+            Console.WriteLine(eredmeny1);
+            Console.WriteLine(eredmeny2);
+
+            #endregion
         }
     }
 }
