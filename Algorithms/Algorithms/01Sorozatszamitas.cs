00e365bc0da07bcb2605f0d4128196aadc9a38ac
Agoston <totha014@gmail.com>
2020-01-23 15:25:24 +0100

    Reformat files

---
+++ Algorithms/Algorithms/01Sorozatszamitas.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms
+{
+    partial class Program
+    {
+        static int Sorozatszamitas(int[] tomb)
+        {
+            int eredmeny = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                eredmeny = eredmeny + tomb[i];
+            }
+
+            return eredmeny;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,30 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms
 {
-    class Program
+    partial class Program
     {
-        static int Sorozatszamitas(int[] tomb)
-        {
-            int eredmeny = 0;
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                eredmeny = eredmeny + tomb[i];
-            }
-
-            return eredmeny;
-        }
-
         static void Main(string[] args)
         {
+            #region Sorozatszamitas teszt
+
             int[] tomb = new int[] { 1, 3, 4, 5, 8 };
             int osszeadasEredmenye = Sorozatszamitas(tomb);
 
             Console.WriteLine(osszeadasEredmenye);
+
+            #endregion
+
+
         }
     }
 }
